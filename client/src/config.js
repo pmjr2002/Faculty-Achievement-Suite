cce42b812801deaecdaed5f36f714b994dd88f22
pmjr2002 <118710442+pmjr2002@users.noreply.github.com>
2024-09-02 22:16:36 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  apiBaseUrl: 'http://localhost:5000/api',
+    apiBaseUrl: 'http://localhost:5000https://faculty-achievement-suite.onrender.com/api',
+
 };
 
 export default config;
